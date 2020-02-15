e436df9f04449dff643fb719d7e7e19229024870
Hernán Galileo Cabrera Garibaldi <galigaribaldi@Macbook.local>
2020-02-15 13:03:40 -0600

    Segunda sesion

--- Proyecto1/Proyecto1/ViewController.swift
+++ Proyecto1/Proyecto1/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var counter: Int = 0
+    //! = boton ya existe
+    
+    ///Label sencillo
+    @IBOutlet weak var Texto: UILabel!
+    
+    ///Boton activador
+    @IBOutlet weak var boton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //view.backgroundColor = .green
+        boton.layer.cornerRadius = 20
     }
 
-
+    //Archivo  XML
+    @IBAction func click(_ sender: UIButton) {
+        counter += 1
+        if counter <= 10{
+        print("Click", counter) //"click \(counter)"
+            Texto.text = "Click \(counter)"
+        }else{
+            boton.backgroundColor = .cyan
+            Texto.text = "Click \(counter)"
+        }
+    }
+    
+    
 }
 
